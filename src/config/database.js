2909d2566a0fc76712b7bf7e05020ba0688b2589
Princekhatak143 <princekhatakpisc@outlook.com>
2024-10-18 23:01:18 +0530

    mongodb connection and post api to insert user data

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDb = async ()=>{
+    await mongoose.connect(
+        "mongodb+srv://princeNodejs:WfCgoNIqbTiYcEap@namastenodjs.8utqg.mongodb.net/devTinder"
+    )
+}
+
+module.exports = connectDb
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const connectDb = require('./config/database')
 const app = express();
-app.listen(8088,()=>{
-    console.log('server is running on port 8088');
+const User = require('./model/user');
+app.use(express.json())
+
+app.post('/signup', async (req,res)=>{
+    const user = new User(req.body)
+
+    try {
+        await user.save();
+        res.send('user added successfully')
+    } catch (error) {
+        res.status(400).send('error saving the user')
+    }
+})
+
+connectDb().then(()=>{
+    console.log('database connection establised');
+    app.listen(8088,()=>{
+        console.log('server is running on port 8088');
+        
+    })
+}).catch((err)=>{
+    console.log('database cannot be connected');
     
 })
