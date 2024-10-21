f4724d135b6338e71ffb0741fe7823832cfe6065
Princekhatak143 <princekhatakpisc@outlook.com>
2024-10-22 00:07:37 +0530

    exploring validator and api

--- src/index.js
+++ src/index.js
@@ -14,7 +14,54 @@ app.post('/signup', async (req,res)=>{
         res.status(400).send('error saving the user')
     }
 })
-  
+app.get('/user', async (req,res)=>{
+     let userEmail =  req.body.emailId
+
+    try {
+       const data =  await User.findOne({emailId:userEmail});
+       
+       if(data !== null){
+
+           res.send(data)
+        }else{
+            res.status(400).send('nodata found')
+        }
+    } catch (error) {
+        res.status(400).send('something went wrong')
+    }
+})
+
+app.patch('/updateUser', async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+
+    const ALLOWED_UPDATES =[
+        "userId",
+        "photoUrl",
+        "gender",
+        "age",
+        "skills"
+    ]
+    const isUpdateAllowed = Object.keys(data).every((k)=>
+        ALLOWED_UPDATES.includes(k)
+    )
+    if(!isUpdateAllowed){
+        res.status(400).send("Update not allowed")
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate({_id: userId},data,{ returnDocument:"before",
+            runValidators:true
+        })
+        console.log(user);
+        res.send('user updated succesfully')
+    } catch (error) {
+        res.status(400).send("Update Failed:"+err.message)
+    }
+   
+    
+})
+
 connectDb().then(()=>{
     console.log('database connection establised');
     app.listen(8088,()=>{
--- src/model/user.js
+++ src/model/user.js
@@ -1,24 +1,64 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4
     },
     lastName: {
         type:String
     },
     emailId : {
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercaes:true,
+        trim:true,
+        validate(value){
+        if(!validator.isEmail(value)){
+        throw new Error("Invalid email:" + value);
+        
+        }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required:true,
+        validate(value){
+if(validator.isStrongPassword(value)){
+throw new Error("Enter a strong Password: " + value)
+}
+        }
     },
     age: {
-        type:Number
+        type:Number,
+        min:18
     },
     gender: {
-        type:String
+        type:String,
+        vaildate(value){
+if(!["male", "female", "other"].includes(value)){
+throw new Error("Gender data is not valid")
+}}
+    },
+    photoUrl:{
+        type:String,
+        default:"",
+        validate(value){
+if(validator.isURL(value)){
+throw new Error("Invalid photo url: " + value)
+}
+        }
+    },
+    skill:{
+        type:[String]
     }
-}) 
+   },
+    {
+        timestamps:true
+    }
+) 
 
 module.exports = mongoose.model('User',userSchema)
